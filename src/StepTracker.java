4530f8e3a8ae1b8dcce54d1491bc48662be563b3
BT_1N3_19 <jlin3392@bths.edu>
2019-05-21 14:37:47 -0400

    finished question 2

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,2 +1,32 @@
 public class StepTracker {
+
+    private int activeMin;
+    private int numDays;
+    private int activeDays;
+    private int totalSteps;
+
+    public StepTracker(int minSteps) {
+        activeMin = minSteps;
+    }
+
+    public void addDailySteps(int steps) {
+        totalSteps += steps;
+        if (steps > activeMin) {
+            activeDays++;
+        }
+        numDays++;
+    }
+
+    public int activeDays() {
+        return activeDays;
+    }
+
+    public double averageSteps() {
+        if (numDays == 0) {
+            return 0.0;
+        } else {
+            return (double)(totalSteps/numDays);
+        }
+    }
+
 }
